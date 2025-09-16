33d505123e77493ab1c304264b0a9056e8df92d7
chenthurr <pchenthurr@gmail.com>
2025-09-16 14:05:47 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import TrafficOpsDashboard from './TrafficOpsDashboard';
+import './index.css'; // Assuming you have a Tailwind CSS setup
+
+function App() {
+  return (
+    <div className="App">
+      <TrafficOpsDashboard />
+    </div>
+  );
+}
+
+export default App;
